5d3db0e0567c8f64ae6a7cbfc3b7e5ffc7f0b529
Joseph-Chege <mjchege23@gmail.com>
2024-04-08 19:54:51 +0300

    Created fuctions that performed the for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        let message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+function countDown(){
+    let i = 10;
+    while (i > -1){
+        console.log(i)
+    i--
+    }
+}
